56ff3085b73044ab36660ab0d88807bae9b49477
lambonas <51776457+lambonas@users.noreply.github.com>
2026-01-31 20:17:59 +1100

    created website design page.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,4 +3,33 @@
 
 import teamHeroImage from './team-hero.png';
 
+// Portfolio images
+import portfolioCabinetMaker1 from './portfolio/cabinet-maker-1.png';
+import portfolioCabinetMaker2 from './portfolio/cabinet-maker-2.png';
+import portfolioEcommerce from './portfolio/ecommerce.png';
+import portfolioElectrician from './portfolio/electrician.png';
+import portfolioFactoryDirectWardrobes from './portfolio/factory-direct-wardrobes.png';
+import portfolioFinanceBroker from './portfolio/finance-broker.png';
+import portfolioGlassReplacementSydney from './portfolio/glass-replacement-sydney.png';
+import portfolioGreenhillsChildcare from './portfolio/greenhills-childcare.png';
+import portfolioLawyer from './portfolio/lawyer.png';
+import portfolioLawyerWebsiteDesign from './portfolio/lawyer-website-design.png';
+import portfolioPodiatrist from './portfolio/podiatrist.png';
+import portfolioWindscreenRepairSydney from './portfolio/windscreen-repair-sydney.png';
+
 export { teamHeroImage };
+
+export {
+  portfolioCabinetMaker1,
+  portfolioCabinetMaker2,
+  portfolioEcommerce,
+  portfolioElectrician,
+  portfolioFactoryDirectWardrobes,
+  portfolioFinanceBroker,
+  portfolioGlassReplacementSydney,
+  portfolioGreenhillsChildcare,
+  portfolioLawyer,
+  portfolioLawyerWebsiteDesign,
+  portfolioPodiatrist,
+  portfolioWindscreenRepairSydney,
+};
